ce6942b8684f4e2e9b5b1ef7d41a82f86b7fafb4
luis <luis@casta.dev>
2026-02-04 06:41:25 -0600

    lc-glaunch/add prefs

--- extension.ts
+++ extension.ts
@@ -11,10 +11,12 @@ export default class GlaunchV2 extends Extension {
 	private _settings: Gio.Settings | null = null;
 	private _launcher: Launcher | null = null;
 	private _signalHandlers: number[] = [];
+	private _displaySignalHandlers: number[] = [];
+	private _settingsChangedIds: number[] = [];
 
 	enable() {
-		this._config = new Config();
 		this._settings = this.getSettings();
+		this._config = new Config(this._settings);
 		this._launcher = new Launcher(this._config, this._settings);
 
 		this._signalHandlers.push(
@@ -27,6 +29,75 @@ export default class GlaunchV2 extends Extension {
 				this._launcher?.deleteApp(win.get_meta_window());
 			})
 		);
+
+		this._displaySignalHandlers.push(
+			global.display.connect("notify::focus-window", () => {
+				this._launcher?.promoteWindow(global.display.focus_window);
+			})
+		);
+
+		// Listen for settings changes to reload keybindings
+		this._settingsChangedIds.push(
+			this._settings.connect("changed::shortcuts", () => {
+				this._reloadShortcuts();
+			})
+		);
+		this._settingsChangedIds.push(
+			this._settings.connect("changed::win-prev-key", () => {
+				this._reloadShortcuts();
+			})
+		);
+		this._settingsChangedIds.push(
+			this._settings.connect("changed::win-other-key", () => {
+				this._reloadShortcuts();
+			})
+		);
+		this._settingsChangedIds.push(
+			this._settings.connect("changed::win-delete-key", () => {
+				this._reloadShortcuts();
+			})
+		);
+		this._settingsChangedIds.push(
+			this._settings.connect("changed::win-center-mouse", () => {
+				this._reloadShortcuts();
+			})
+		);
+	}
+
+	private _reloadShortcuts() {
+		if (!this._settings) return;
+
+		console.debug("[GlaunchV2] Settings changed, reloading shortcuts...");
+
+		// Unbind all current keybindings
+		this._config?.entries.forEach((bind) => {
+			if (bind.key) {
+				Main.wm.removeKeybinding(bind.key);
+			}
+		});
+
+		// Reload config and launcher
+		this._config = new Config(this._settings);
+		this._launcher = new Launcher(this._config, this._settings);
+
+		console.debug("[GlaunchV2] Shortcuts reloaded");
+
+		// Refocus the Extensions preferences window
+		this._focusExtensionsWindow();
+	}
+
+	private _focusExtensionsWindow() {
+		const windows = global.display.get_tab_list(Meta.TabList.NORMAL, null);
+		for (const win of windows) {
+			const wmClass = win.get_wm_class();
+			// Match GNOME Extensions app or extension-prefs window
+			if (wmClass && (wmClass.includes("Extensions") || wmClass.includes("extension-prefs"))) {
+				win.raise_and_make_recent_on_workspace(win.get_workspace());
+				win.focus(global.get_current_time());
+				console.debug("[GlaunchV2] Refocused Extensions window");
+				return;
+			}
+		}
 	}
 
 	disable() {
@@ -34,6 +105,17 @@ export default class GlaunchV2 extends Extension {
 			global.window_manager.disconnect(id);
 		});
 		this._signalHandlers = [];
+
+		this._displaySignalHandlers.forEach(id => {
+			global.display.disconnect(id);
+		});
+		this._displaySignalHandlers = [];
+
+		// Disconnect settings change handlers
+		this._settingsChangedIds.forEach(id => {
+			this._settings?.disconnect(id);
+		});
+		this._settingsChangedIds = [];
 
 		this._config?.entries.forEach((bind, _) => {
 			if (bind.key) {
